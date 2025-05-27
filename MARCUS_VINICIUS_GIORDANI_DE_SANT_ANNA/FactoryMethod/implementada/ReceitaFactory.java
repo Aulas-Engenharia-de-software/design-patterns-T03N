2ddc141ae27cf2e44b0b9cdf617ae80164285a36
MarcusGiordani <marcus.giordani15@gmail.com>
2025-05-26 22:05:20 -0300

    Factory Method e Strategy

---
+++ MARCUS_VINICIUS_GIORDANI_DE_SANT_ANNA/FactoryMethod/implementada/ReceitaFactory.java
@@ -0,0 +1,5 @@
+package MARCUS_VINICIUS_GIORDANI_DE_SANT_ANNA.FactoryMethod.implementada;
+
+public abstract class ReceitaFactory {
+    public abstract Receita criarReceita();
+}
---
+++ MARCUS_VINICIUS_GIORDANI_DE_SANT_ANNA/FactoryMethod/implementada/ReceitaFitness.java
@@ -0,0 +1,7 @@
+package MARCUS_VINICIUS_GIORDANI_DE_SANT_ANNA.FactoryMethod.implementada;
+
+public class ReceitaFitness extends Receita {
+    public void preparar() {
+        System.out.println("Preparando: Panqueca de aveia");
+    }
+}
